9fa1dfb22130d649ea77b6b9fe509a1b8fcd7f3d
TkTnX <timur20-09@mail.ru>
2024-05-27 19:09:39 +0500

    env

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://3943829923b5c84f.mokky.dev",
+  baseURL: process.env.NEXT_PUBLIC_MOKKY_URL,
 });
 
 export default instance;

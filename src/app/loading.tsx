25bec8783c4b309b810dace93a44b12e6e861a0f
TkTnX <timur20-09@mail.ru>
2024-05-30 22:53:29 +0500

    страница загрузки

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="absolute left-0 top-0 right-0 bottom-0 bg-purple-800 flex items-center justify-center z-30">
+      Загрузка...
+    </div>
+  );
+};
+
+export default loading;
